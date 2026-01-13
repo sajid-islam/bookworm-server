2299035c0195f0762667b017695a6f6085327ce1
sajid-islam <sajidislam941@gmail.com>
2026-01-13 23:35:42 +0600

    route- /book/:id(delete): created

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -105,4 +105,20 @@ router.put('/:id', verifyToken, verifyAdmin, async (req, res) => {
   }
 });
 
+router.delete('/:id', verifyToken, verifyAdmin, async (req, res) => {
+  const bookId = req.params.id;
+  try {
+    const book = await Book.findByIdAndDelete(bookId);
+
+    if (!book) {
+      return res.status(404).json({ message: 'Book not found' });
+    }
+
+    res.status(200).json({ message: 'Book deleted permanently' });
+  } catch (error) {
+    console.error('Error deleting book', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
 export default router;
