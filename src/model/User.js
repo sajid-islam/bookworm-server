c6f732e5b57c9368cc4ed9ad3e664c9a01fe797b
sajid-islam <sajidislam941@gmail.com>
2026-01-13 22:48:40 +0600

    user-schema: add new filed role

--- src/model/User.js
+++ src/model/User.js
@@ -7,6 +7,7 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true },
     photo: { type: String, required: true },
     password: { type: String, required: true },
+    role: { type: String, default: 'user' },
   },
   { timestamps: true }
 );
