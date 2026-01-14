82dd2493594a9f3b9cf4b93b0378c3c5f221d4ce
sajid-islam <sajidislam941@gmail.com>
2026-01-14 07:06:10 +0600

    schema: add Tutorial schema

---
+++ src/model/Tutorial.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const tutorialSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      trim: true,
+    },
+
+    youtubeUrl: {
+      type: String,
+      required: true,
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Tutorial = mongoose.model('Tutorial', tutorialSchema);
